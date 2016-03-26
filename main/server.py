170d9828aea1162261c34c339f03528a21b7c1f0
430Train <mdesorme@mail.umw.edu>
2016-03-26 10:26:54 +0000

    Added a bunch of files that weren't a part of the repo including server.py

--- main/server.py
+++ main/server.py
@@ -1,9 +1,14 @@
 #this is the file that starts the server and has all of the server commands in it
+#!/usr/bin/env python
 
 import psycopg2
 import psycopg2.extras
 import os
 from flask import Flask, session, render_template, request, redirect, url_for
+import cgi, cgitb
+
+# may need this for later
+#cgitb.enable()
 
 app = Flask(__name__)
 
@@ -12,6 +17,7 @@ app.secret_key = os.urandom(24).encode('hex')
 loginError = False
 verifiedUser = ''
 userType = ''
+
 
 #connectToDB--------------------------------------------------------------------------------------------
 def connectToDB():
@@ -225,37 +231,189 @@ def adminExercisesPage():
         verifiedUser = ''
     db = connectToDB()
     cur = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    # get all exercises from the database ...
+    rows = []
+    query = "select exercise_name, muscle_group, youtube_link from exercises"
+    print query
+    cur.execute("select exercise_name, muscle_group, youtube_link from exercises")
+    #if cur.fetchone():
+    rows = cur.fetchall()
+    print(rows)
+    ## For dubugging ##
+    #print(rows[0][0])
+    
+    if userType == 'admin':
+        # getting the user's first and last name(only admins)
+        cur.execute("SELECT first_name, last_name FROM admin WHERE user_name = %s", (verifiedUser,)) #<- make sure if there is only one variable, it still needs a comma for some reason
+        names=cur.fetchall()
+        print(names)
+    
     # if user typed in a post ...
     if request.method == 'POST':
         print "HI"
-        session['username'] = request.form['username']
-        print(session['username'])
+        print "made it to post"
+        
+        #This is where we iterate through all exercises found in the database to see which one was selected.
+        action = request.form['action']
+        exercise = request.form['exercise']
+        print(action)
+        print(exercise)
+        if action == 'Edit':
+            session['exercise'] = exercise
+            return redirect(url_for('adminEditExercisesPage'))
+        if action == 'Delete':
+            confirm = request.form['confirmD']
+            print(confirm)
+            if confirm == 'Delete':
+                query = "DELETE FROM exercises WHERE exercise_name = '%s'" % (exercise,)
+                print(query)
+                try:
+                    cur.execute("DELETE FROM exercises WHERE exercise_name = %s", (exercise,))
+                except:
+                    print("Problem inserting into exercises")
+                    db.rollback()
+                db.commit()
+            return redirect(url_for('adminExercisesPage'))
+        if action == 'View':
+            session['exercise'] = exercise
+            return redirect(url_for('adminViewExercisesPage'))
+        
+    #user and userType are being passed to the website here along with the exercise data as "results".
+    return render_template('Theme/aExercises.html', user = verifiedUser, userType = userType, Name = names, results = rows)
+#end admin exercises page--------------------------------------------------  
 
-        pw = request.form['pw']
-        query = "select * from users WHERE username = '%s' AND password = crypt('%s', password)" % (session['username'], pw)
-        print query
-        cur.execute("select * from users WHERE username = %s AND password = crypt(%s, password)", (session['username'], pw))
-        if cur.fetchone():
-            verifiedUser = session['username']
-            return redirect(url_for('adminHome'))
-        else:
-            verifiedUser = ''
-            session['username'] = ''
-            
-    # get exercises from the database
-    rows=[]
-    cur.execute('SELECT exercise_name, muscle_group, youtube_link FROM exercises')
-    rows=cur.fetchall()
+#admin view exercise page------------------------------------------------------    
+@app.route('/aViewExercises', methods=['GET', 'POST'])
+def adminViewExercisesPage():
+    if 'username' in session:
+        verifiedUser = session['username']
+    else:
+        verifiedUser = ''
+    if 'userType' in session:
+        userType = session['userType']
+    else:
+        userType = ''
+    if verifiedUser == '':
+        return redirect(url_for('login'))
+    if userType == '':
+        return redirect(url_for('login'))
+    if 'username' in session:
+        verifiedUser = session['username']
+    else:
+        verifiedUser = ''
+    if 'exercise' in session:
+        exercise = session['exercise']
+    else:
+        return redirect(url_for('adminExercisesPage'))
+    db = connectToDB()
+    cur = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    # get all data for the exercise ...
+    rows = []
+    query = "select exercise_name, muscle_group, description, youtube_link from exercises where exercise_name = '%s'" % (exercise,)
+    print query
+    cur.execute("select exercise_name, muscle_group, description, youtube_link from exercises where exercise_name = %s",(exercise,))
+    rows = cur.fetchall()
+    print(rows)
+    ## For dubugging ##
+    #print(rows[0][0])
+    
+    if userType == 'admin':
+        # getting the user's first and last name(only admins)
+        cur.execute("SELECT first_name, last_name FROM admin WHERE user_name = %s", (verifiedUser,)) #<- make sure if there is only one variable, it still needs a comma for some reason
+        names=cur.fetchall()
+        print(names)
+    
+    #user, userType, names, and all data for the exercise are being passed to the website here. 
+    return render_template('Theme/aViewExercises.html', user = verifiedUser, userType = userType, Name = names, results = rows, exercise = exercise)
+#end admin view exercises page-------------------------------------------------- 
 
+#admin edit exercise page------------------------------------------------------    
+@app.route('/aEditExercises', methods=['GET', 'POST'])
+def adminEditExercisesPage():
+    if 'username' in session:
+        verifiedUser = session['username']
+    else:
+        verifiedUser = ''
+    if 'userType' in session:
+        userType = session['userType']
+    else:
+        userType = ''
+    if verifiedUser == '':
+        return redirect(url_for('login'))
+    if userType == '':
+        return redirect(url_for('login'))
+    if 'username' in session:
+        verifiedUser = session['username']
+    else:
+        verifiedUser = ''
+    if 'exercise' in session:
+        exercise = session['exercise']
+    else:
+        return redirect(url_for('adminExercisesPage'))
+    db = connectToDB()
+    cur = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    badName = False
+    exerciseEdited = False
+    
+    # get all data for the exercise ...
+    rows = []
+    query = "select exercise_name, muscle_group, description, youtube_link from exercises where exercise_name = '%s'" % (exercise,)
+    print query
+    cur.execute("select exercise_name, muscle_group, description, youtube_link from exercises where exercise_name = %s",(exercise,))
+    rows = cur.fetchall()
+    print(rows)
+    ## For dubugging ##
+    #print(rows[0][0])
+    
     if userType == 'admin':
         # getting the user's first and last name(only admins)
         cur.execute("SELECT first_name, last_name FROM admin WHERE user_name = %s", (verifiedUser,)) #<- make sure if there is only one variable, it still needs a comma for some reason
         names=cur.fetchall()
         print(names)
         
-    #user and userType are being passed to the website here
-    return render_template('Theme/aExercises.html', user = verifiedUser, userType = userType, Name = names, results=rows)
-#end admin exercises page--------------------------------------------------  
+    if request.method == 'POST':
+        print "HI"
+        
+        exerciseName = request.form['ename']
+        print(exerciseName)
+        muscleGroup = request.form['emusclegroup']
+        print(muscleGroup)
+        description = request.form['edesc']
+        print(description)
+        youTube = request.form['eyoutube']
+        print(youTube)
+        
+        query = "select exercise_name from exercises WHERE exercise_name = '%s'" % (exerciseName,)
+        print query
+        cur.execute("select exercise_name from exercises WHERE exercise_name = %s", (exerciseName,))
+        if cur.fetchone() and exerciseName != exercise:
+            badName = True
+        elif exerciseName == '':
+            badName = True
+        else:
+            badName = False
+            exerciseEdited = True
+            query = "UPDATE exercises SET (admin_id, exercise_name, description, muscle_group, youtube_link) = ('%s', '%s', '%s', '%s', '%s') WHERE exercise_name = '%s'" % (session['ID'],exerciseName,description,muscleGroup,youTube,exercise)
+            print query
+            try:
+                cur.execute("UPDATE exercises SET (admin_id, exercise_name, description, muscle_group, youtube_link) = (%s, %s, %s, %s, %s) WHERE exercise_name = %s", (session['ID'],exerciseName,description,muscleGroup,youTube,exercise))
+            except:
+                print("Problem updating exercises")
+                db.rollback()
+            db.commit()
+            session['exercise'] = exerciseName
+            exercise = exerciseName
+            # get the newly updated exercies ready for display
+            cur.execute("select exercise_name, muscle_group, description, youtube_link from exercises where exercise_name = %s",(exercise,))
+            rows = cur.fetchall()
+            print("done!")
+            #return redirect(url_for('adminEditExercisesPage'))
+    print(exerciseEdited)    
+    #user, userType, names, and all data for the exercise are being passed to the website here. badName and exerciseEdited are for error and success notification.
+    return render_template('Theme/aEditExercises.html', user = verifiedUser, userType = userType, Name = names, results = rows, exercise = exercise, badName = badName, exerciseEdited = exerciseEdited)
+#end admin edit exercises page--------------------------------------------------
 
 #admin create exercises page------------------------------------------------------    
 @app.route('/aCreateExercise', methods=['GET', 'POST'])
@@ -279,6 +437,8 @@ def adminCreateExercisePage():
     db = connectToDB()
     cur = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
     badName = False
+    exerciseCreated = False
+
     # if user typed in a post ...
     if request.method == 'POST':
         print "HI"
@@ -297,9 +457,12 @@ def adminCreateExercisePage():
         cur.execute("select exercise_name from exercises WHERE exercise_name = %s", (exerciseName,))
         if cur.fetchone():
             badName = True
-            return redirect(url_for('adminCreateExercisePage'))
+            #return redirect(url_for('adminCreateExercisePage'))
+        elif exerciseName == '':
+            badName = True
         else:
             badName = False
+            exerciseCreated = True
             query = "INSERT INTO exercises (admin_id, exercise_name, description, muscle_group, youtube_link) VALUES ('%s', '%s', '%s', '%s', '%s')" % (session['ID'],exerciseName,description,muscleGroup,youTube)
             print query
             try:
@@ -317,8 +480,8 @@ def adminCreateExercisePage():
         names=cur.fetchall()
         print(names)
         
-    #user and userType are being passed to the website here
-    return render_template('Theme/aCreateExercise.html', user = verifiedUser, userType = userType, Name = names, badName = badName)
+    #user and userType are being passed to the website here. badName and exerciseCreated are for error and success notification.
+    return render_template('Theme/aCreateExercise.html', user = verifiedUser, userType = userType, Name = names, badName = badName, exerciseCreated = exerciseCreated)
 #end admin create exercise page--------------------------------------------------
 
 #admin workouts page------------------------------------------------------    
@@ -368,54 +531,6 @@ def adminWorkoutsPage():
     #user and userType are being passed to the website here
     return render_template('Theme/aWorkouts.html', user = verifiedUser, userType = userType, Name = names)
 #end admin workout page--------------------------------------------------  
-
-#admin training programs page------------------------------------------------------    
-@app.route('/aTrainingPrograms', methods=['GET', 'POST'])
-def adminTrainingProgramsPage():
-    if 'username' in session:
-        verifiedUser = session['username']
-    else:
-        verifiedUser = ''
-    if 'userType' in session:
-        userType = session['userType']
-    else:
-        userType = ''
-    if verifiedUser == '':
-        return redirect(url_for('login'))
-    if userType == '':
-        return redirect(url_for('login'))
-    if 'username' in session:
-        verifiedUser = session['username']
-    else:
-        verifiedUser = ''
-    db = connectToDB()
-    cur = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    # if user typed in a post ...
-    if request.method == 'POST':
-        print "HI"
-        session['username'] = request.form['username']
-        print(session['username'])
-
-        pw = request.form['pw']
-        query = "select * from users WHERE username = '%s' AND password = crypt('%s', password)" % (session['username'], pw)
-        print query
-        cur.execute("select * from users WHERE username = %s AND password = crypt(%s, password)", (session['username'], pw))
-        if cur.fetchone():
-            verifiedUser = session['username']
-            return redirect(url_for('adminHome'))
-        else:
-            verifiedUser = ''
-            session['username'] = ''
-
-    if userType == 'admin':
-        # getting the user's first and last name(only admins)
-        cur.execute("SELECT first_name, last_name FROM admin WHERE user_name = %s", (verifiedUser,)) #<- make sure if there is only one variable, it still needs a comma for some reason
-        names=cur.fetchall()
-        print(names)
-        
-    #user and userType are being passed to the website here
-    return render_template('Theme/aTrainingPrograms.html', user = verifiedUser, userType = userType, Name = names)
-#end admin training program page-------------------------------------------------- 
 
 #admin create workout page------------------------------------------------------    
 @app.route('/aCreateWorkout', methods=['GET', 'POST'])
